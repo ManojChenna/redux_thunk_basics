9bb2cf5f1f1e6048033361545f71867237a67fc2
Manoj Chennareddy <52932901+ManojChenna@users.noreply.github.com>
2020-12-13 18:08:56 +0530

    memorize user call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
 
 //Action creaters
 
@@ -8,8 +9,10 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: resp.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const resp = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: resp.data });
-};
+});
