45bd24789bccf6fc1d5d307989e32093002ee572
Manoj Chennareddy <52932901+ManojChenna@users.noreply.github.com>
2020-12-13 18:28:28 +0530

    Alternate fetch user

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,16 +3,28 @@ import _ from "lodash";
 
 //Action creaters
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async (dispatch) => {
   const resp = await jsonPlaceholder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: resp.data });
 };
 
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const resp = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: resp.data });
-});
+};
+
+// Memorized verison
+//export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const resp = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: resp.data });
+// });
